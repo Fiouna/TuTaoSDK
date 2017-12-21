1f50aa3d974990bc69f7eb3483c4de9695cfcece
zhang <wen_9398@163.com>
2017-12-21 14:36:03 +0800

    add TuTaoSDK

---
+++ TuTaoSDK.framework/Headers/TuTaoLog.h
@@ -0,0 +1,64 @@
+//
+//  TuTaoLog.h
+//  TuTaoSDK
+//
+//  Created by Tm on 2017/12/20.
+//  Copyright © 2017年 Tim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  日志输出级别
+ */
+typedef NS_ENUM(NSUInteger, TuTaoLogLevel){
+    //关闭输出
+    TuTaoLogLevelOff       = 0,
+    //仅输出错误
+    TuTaoLogLevelError     = 1,
+    //输出警告错误
+    TuTaoLogLevelWarning   = 2,
+    //输出INFO警告错误
+    TuTaoLogLevelInfo      = 3,
+    //输出调试信息
+    TuTaoLogLevelDebug     = 4,
+};
+
+
+@interface TuTaoLog : NSObject
+
+/**
+ *  日志输出级别 默认TuTaoLogLevelOff
+ */
+@property (nonatomic) TuTaoLogLevel level;
+
+/**
+ *  日志处理类
+ */
++ (TuTaoLog *)sharedLogger;
+
+
+/**
+ *  输出日志
+ *
+ *  @param format 日志信息
+ */
++ (void)log:(NSString *)format, ...;
++ (void)error:(NSString *)format, ...;
++ (void)warning:(NSString *)format, ...;
++ (void)info:(NSString *)format, ...;
++ (void)debug:(NSString *)format, ...;
+
+@end
+
+
+#ifndef TuTaoSDKLogDefine
+#define TuTaoSDKLogDefine
+
+#define kTLog(...) [TuTaoLog log:__VA_ARGS__]
+#define kTLogError(...) [TuTaoLog error:__VA_ARGS__]
+#define kTLogWarning(...) [TuTaoLog warning:__VA_ARGS__]
+#define kTLogInfo(...) [TuTaoLog info:__VA_ARGS__]
+#define kTLogDebug(...) [TuTaoLog debug:__VA_ARGS__]
+
+#endif
---
+++ TuTaoSDK.framework/Headers/TuTaoManager.h
@@ -0,0 +1,34 @@
+//
+//  TuTaoManager.h
+//  TuTaoSDK
+//
+//  Created by Tm on 2017/12/13.
+//  Copyright © 2017年 Tim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TuTaoLog.h"
+
+@interface TuTaoManager : NSObject
+
+//打印SDK版本
++ (void)version;
+
+/**
+ *  初始化SDK
+ *
+ *  @param appkey 应用秘钥
+ */
++ (void)initWithAppKey:(NSString *)appkey;
+
+/**
+ *  设置日志输出级别
+ *
+ *  @param level 日志输出级别 默认TuTaoLogLevelOff
+ */
++ (void)setLogLevel:(TuTaoLogLevel)level;
+
+//暂无处理事件，目前只做测试
++ (void)start;
+
+@end
